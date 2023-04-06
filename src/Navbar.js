f393dc0f61a52eca9221caed8e45409964eaaab2
Rachana Shrestha <rachnastha912@gmail.com>
2023-04-06 13:51:12 +0545

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1> THe Dojo Blog</h1>
+            <div classname="links">
+                <a href="/">Home</a>
+                <a href="/create">Newblog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
